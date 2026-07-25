eab7c9d31dd931e0d4f708067fdc6b4eb444c0e4
Akhila Gururaj <akhilacvsmg@gmail.com>
2026-07-25 13:34:36 +0530

    Completed Day01 Python program

--- Python/Day01/day01.py
+++ Python/Day01/day01.py
@@ -1,1 +1,13 @@
-day01.py
+print("Welcome to my AI Journey!")
+
+name = "Akhila"
+age = 44
+profession = "Assistant Professor"
+dream_job = "Applied AI Engineer"
+experience ="9.5 years"
+
+print("Name:", name)
+print("Age:", age)
+print("Profession:", profession)
+print("Dream Job:", dream_job)
+print("Experience:", experience)
