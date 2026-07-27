00f6561ba93103956a474b40bf9f8465f58a3f2f
Akhila Gururaj <akhilacvsmg@gmail.com>
2026-07-27 15:40:56 +0530

    Extended Program saved

--- Python/Day02/program1.py
+++ Python/Day02/program1.py
@@ -1,5 +1,16 @@
 name=input("Enter your name : ")
-age=int(input("Enter your Age :"))
-dream=input("Enter your dream job")
+age=int(input("Enter your age :"))
+dream=input("Enter your dream job : ")
+Collegename =input("Enter your College Name : ")
+Department= input("Enter the department : ")
+CGPA=float(input("Enter the CGPA : "))
 
-print(f"My name is {name},I am {age} old and I want to be a {dream}")
+print("\n\n")
+print("-------------PROFILE----------")
+
+
+print(f"My name is {name}\nI am {age} old \nI want to be a {dream}")
+print(f"I studied from {Collegename}\nDepartment :{Department}\nwith CGPA :{CGPA}")
+
+
+print("----\n\nThank you!!!!\n\n ")
